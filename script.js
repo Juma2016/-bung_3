277950e3e43099a4f682de746902d471d30f5e3a
Mario Shenouda <mario.shenouda@edu.fh-joanneum.at>
2025-05-12 11:04:23 +0200

    bearbeiten an script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 console.log("Skript geladen!");
 
-// 1) Überschrift ändern
+
 document.getElementById("headline").addEventListener("click", () => {
   console.log("Überschrift angeklickt");
   document.getElementById("headline").innerText = "Überschrift wurde geändert!";
 });
 
-// 2) Bild tauschen
+
 const img = document.getElementById("image");
 img.addEventListener("mouseover", () => {
   console.log("Maus über Bild");
@@ -17,7 +17,7 @@ img.addEventListener("mouseout", () => {
   img.src = "bild1.png";
 });
 
-// 3) Neuen Text hinzufügen
+
 document.getElementById("addTextBtn").addEventListener("click", () => {
   console.log("Button 'Neuen Text hinzufügen' geklickt");
   const p = document.createElement("p");
@@ -25,13 +25,13 @@ document.getElementById("addTextBtn").addEventListener("click", () => {
   document.getElementById("textContainer").appendChild(p);
 });
 
-// 4a) Listentitel ändern
+
 document.getElementById("listTitle").addEventListener("click", () => {
   console.log("Liste-Überschrift angeklickt");
   document.getElementById("listTitle").innerText = "Titel der Liste geändert";
 });
 
-// 4b) Liste sortieren
+
 document.getElementById("sortBtn").addEventListener("click", () => {
   console.log("Button 'Liste alphabetisch sortieren' geklickt");
   const ul = document.getElementById("itemList");
